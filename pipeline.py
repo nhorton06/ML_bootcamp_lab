2f774160e2b040e7138c95c9024fd02a0da22891
nhorton06 <nathaniel.horton06@gmail.com>
2026-02-04 01:55:51 +0000

    Finish pipelines and add appropriate comments

--- pipeline.py
+++ pipeline.py
@@ -19,31 +19,36 @@ from sklearn.preprocessing import StandardScaler
 college = pd.read_csv('https://github.com/UVADS/DS-3021/blob/main/data/cc_institution_details.csv')
 
 # Drop ID variables and unnecessary columns (and maybe missing values)
-def drop_cols(data, drop_cols = [], drop_missing = False):
-    for col in drop_cols:
-        data = data.drop(columns = [col])
+def drop_cols_college(data, drop_cols = [], drop_missing = False):
+    '''Drop identifier variables and unnecessary columns (and optionally missing values since there are some in this dataset)'''
+    if drop_cols:
+        data = data.drop(columns = drop_cols)
     if drop_missing:
         data = data.dropna()
     return data
 
 # Change to category type
 def type_cat_college(data, cat_cols = []):
+    '''Change given to columns to type category'''
     for col in cat_cols:
         data[col] = data[col].astype('category')
     return data
 
 # One-hot encoding
 def onehot_encode_college(data, cat_cols = []):
+    '''One-hot encode specified categorical columns'''
     return pd.get_dummies(data, columns = cat_cols)
 
 # Create target variable and calculate prevalence
 def create_target_college(data, source_col, target_name, threshold = 0.5):
-    data[target_name] = (data[source_col] >= threshold).astype(int)
+    '''Create binary target variable and calculate prevalence, threshold can be changed to what user sees fit'''
+    data[target_name] = (data[source_col] >= threshold).astype(int) # defined target_name for new target variable should be used for any further reference of target_var in further functions
     prevalence = data[target_name].mean()
     return data, prevalence
 
 # Normalize/scale numeric data
 def scale_numeric_college(data, target_var, numeric_cols = None):
+    '''Scale numeric columns using StandardScaler, but excludes specified target variable'''
     if numeric_cols == None:
         numeric_cols = data.select_dtypes(include = ['int64', 'float64']).columns.tolist()
         numeric_cols = [col for col in numeric_cols if col != target_var]
@@ -52,7 +57,8 @@ def scale_numeric_college(data, target_var, numeric_cols = None):
     return data
 
 # Split for train, tune, test
-def split_college(data, target_var, test_size = 0.2, tune_size = 0.25, stratify = True):
+def split_college(data, target_var, test_size = 0.2, tune_size = 0.25, stratify = True):  # test sizes and tune sizes are adjustable
+    '''Splits data into train, tune, and test sets, with stratification of target variable on by default'''
     stratify_col = data[target_var] if stratify else None
     train, temp = train_test_split(data, test_size = test_size, random_state = 42, stratify = stratify_col)
     stratify_temp = temp[target_var] if stratify else None
@@ -72,3 +78,55 @@ def split_college(data, target_var, test_size = 0.2, tune_size = 0.25, stratify 
 # %%
 
 # Job Placement Dataset
+
+# Read in data
+job = pd.read_csv('https://raw.githubusercontent.com/DG1606/CMS-R-2020/master/Placement_Data_Full_Class.csv')
+
+# Drop ID variables and unnecessary columns
+def drop_cols_job(data, drop_cols = []):
+    '''Drop identifier variables and unnecessary columns'''
+    if drop_cols:
+        data = data.drop(columns = drop_cols)
+    return data
+
+# Change to category type
+def type_cat_job(data, cat_cols = []):
+    '''Change given to columns to type category'''
+    for col in cat_cols:
+        data[col] = data[col].astype('category')
+    return data
+
+# One-hot encoding
+def onehot_encode_job(data, cat_cols = []):
+    '''One-hot encode specified categorical columns'''
+    return pd.get_dummies(data, columns = cat_cols)
+
+# Adjust target variable (already exists as placed/not placed, but can be made to 1/0 for easier computing) and calculate prevalence
+def adjust_target_job(data, source_col, success): # source_col should be 'status' for this dataset in particular, positive should be 'Placed'
+    '''Adjusts target variable ('status' in this case) to be binary to 1 and 0 given the success string'''
+    data[source_col] = data[source_col].apply(lambda x: 1 if x == success else 0)
+    prevalence = data[source_col].mean()
+    return data, prevalence
+
+# Normalize/scale numeric data
+def scale_numeric_job(data, target_var, numeric_cols = None):
+    '''Scale numeric columns using StandardScaler, but excludes specified target variable'''
+    if numeric_cols == None:
+        numeric_cols = data.select_dtypes(include = ['int64', 'float64']).columns.tolist()
+        numeric_cols = [col for col in numeric_cols if col != target_var]
+    scaler = StandardScaler()
+    data[numeric_cols] = scaler.fit_transform(data[numeric_cols])
+    return data
+
+# Split for train, tune, test
+def split_job(data, target_var, test_size = 0.2, tune_size = 0.25, stratify = True): # target_var should be 'status' for this dataset, test sizes and tune sizes are adjustable
+    '''Splits data into train, tune, and test sets, with stratification of target variable on by default'''
+    stratify_col = data[target_var] if stratify else None
+    train, temp = train_test_split(data, test_size = test_size, random_state = 42, stratify = stratify_col)
+    stratify_temp = temp[target_var] if stratify else None
+    tune, test = train_test_split(temp, test_size = 0.5, random_state = 42, stratify = stratify_temp)
+
+    print(f'Training set shape: {train.shape}')
+    print(f'Tuning set shape: {tune.shape}')
+    print(f'Testing set shape: {test.shape}')
+    return train, tune, test
--- questions.py
+++ questions.py
@@ -8,7 +8,8 @@
 # - **Job Placement Dataset**: How do factors like degree, specialization, and gender influence job placement rates after graduation?
 
 
-
+# -------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
 
 
 
@@ -37,7 +38,7 @@ college = pd.read_csv('https://github.com/UVADS/DS-3021/blob/main/data/cc_instit
 college = college.drop(columns = ['chronname', 'site', 'nicknames'])
 
 # %%
-# Deal with missing values (these columns have a TON of missing values, so dropping them seems better)
+# Deal with missing values (these columns have a TON of missing values, so dropping them seems better than imputing or anything else)
 college = college.drop(columns = [
     'vsa_year', 'vsa_grad_after4_first', 'vsa_grad_elsewhere_after4_first', 
     'vsa_enroll_after4_first', 'vsa_enroll_elsewhere_after4_first', 'vsa_grad_after6_first',
@@ -48,23 +49,23 @@ college = college.drop(columns = [
     'endow_value', 'endow_percentile'])
 
 # %%
-# Type corrections
+# Type corrections from string to category
 cat_cols = ['control', 'city', 'state', 'level']
 for col in cat_cols:
     college[col] = college[col].astype('category')
 
 # %%
-# Collapse factor levels
+# Collapse factor levels (get rid of Non-profit for profit private schools, just group them both into private schools for simplicity and making it binary)
 college['control'] = college['control'].apply(
     lambda x: 'Public' if x == 'Public' else 'Private')
 
 # %%
-# One-hot encoding
+# One-hot encoding categorical variables for model
 cat_cols = ['control', 'city', 'state', 'level']
 college = pd.get_dummies(college, columns = cat_cols)
 
 # %%
-# Create target variable
+# Create target variable with 0.5 as cutoff (can easily be changed, just set 0.5 as a default), 1 if true, 0 if not
 college['target_completion'] = (college['fte_percentile'] >= 0.5).astype(int)
 
 # %%
@@ -72,37 +73,34 @@ college['target_completion'] = (college['fte_percentile'] >= 0.5).astype(int)
 prevalence = college['target_completion'].mean()
 
 # %%
-# Scale numeric data
+# Scale numeric data to mean 0 and std dev 1 to ensure equal influence across numeric features
 numeric_cols = college.select_dtypes(include = ['int64', 'float64']).columns.tolist()
 numeric_cols = [col for col in numeric_cols if col != 'target_completion']
 scaler = StandardScaler()
 college[numeric_cols] = scaler.fit_transform(college[numeric_cols])
 
 # %%
-# Train, tune, test split
+# Train, tune, test split (train 60%, tune 20%, test 20% with given parameters)
 train_data, test_data = train_test_split(college, test_size = 0.2, random_state = 42)
 train_data, tune_data = train_test_split(train_data, test_size = 0.25, random_state = 42)
 
 
 
-
+# -------------------------------------------------------------------------------------------------
 
 
 
 # %%[markdown]
 # #### Job Placement Data
 
-# %%
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+# Libraries already imported above
 
 # %%
 # Read in data
 job_placement = pd.read_csv('https://raw.githubusercontent.com/DG1606/CMS-R-2020/master/Placement_Data_Full_Class.csv')
 
 # %%
-# Drop unnecessary columns
+# Drop unnecessary columns (sl_no is just an index label, not necessary)
 job_placement = job_placement.drop(columns = ['sl_no'])
 
 # %%
@@ -110,13 +108,13 @@ job_placement = job_placement.drop(columns = ['sl_no'])
 job_placement['salary'] = job_placement['salary'].fillna(0)
 
 # %%
-# Type corrections
+# Type corrections (string to category)
 cat_cols = ['gender', 'ssc_b', 'hsc_b', 'hsc_s', 'degree_t', 'workex', 'specialisation']
 for col in cat_cols:
     job_placement[col] = job_placement[col].astype('category')
 
 # %%
-# One-hot encoding
+# One-hot encoding categorical variables for model
 cat_cols = ['gender', 'ssc_b', 'hsc_b', 'hsc_s', 'degree_t', 'workex', 'specialisation']
 job_placement = pd.get_dummies(job_placement, columns = cat_cols)
 
@@ -124,13 +122,13 @@ job_placement = pd.get_dummies(job_placement, columns = cat_cols)
 # levels since the categories are already pretty distinct.
 
 # %%
-# Normalize continuous variables
+# Normalize continuous variables to mean 0 and std dev 1
 numeric_cols = job_placement.select_dtypes(include = ['int64', 'float64']).columns.tolist()
 numeric_cols = [col for col in numeric_cols if col != 'status']
 scaler = StandardScaler()
 job_placement[numeric_cols] = scaler.fit_transform(job_placement[numeric_cols])
 
-# Adjust target variable column to be 0/1
+# Adjust target variable column to be 0/1 for easier computation for model
 job_placement['status'] = job_placement['status'].apply(lambda x: 1 if x == 'Placed' else 0)
 
 # %%
@@ -138,13 +136,14 @@ job_placement['status'] = job_placement['status'].apply(lambda x: 1 if x == 'Pla
 prevalence = job_placement['status'].mean()
 
 # %%
-# Train, tune, test split
+# Train, tune, test split (again train 60%, tune 20%, test 20%)
 train_data, test_data = train_test_split(job_placement, test_size = 0.2, random_state = 42)
 train_data, tune_data = train_test_split(train_data, test_size = 0.25, random_state = 42)
 
 
 
-
+# -------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
 
 
 # %% [markdown]
